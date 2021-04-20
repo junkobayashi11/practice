88c7c81162679b96f847f1f75e5d5970d8cd24c7
junkobayashi11 <77084304+junkobayashi11@users.noreply.github.com>
2021-04-20 15:45:24 +0900

    feat: stateの基礎

--- torazemi/src/Article.jsx
+++ torazemi/src/Article.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const Article = (props) => {
-  let publishState = "";
-  if(props.isPublished){
-    publishState = "非公開"
-  }else{
-    publishState = "公開"
-  }
+  // let publishState = "";
+  // if(props.isPublished){
+  //   publishState = "非公開"
+  // }else{
+  //   publishState = "公開"
+  // }
   return(
     <div>
       <h2>{props.title}</h2>
-      <p>順番は：{props.order}です</p>
+      <label htmlFor="check">公開状態：</label>
+      <input type="checkbox" checked= {props.isPublished} id="check" onClick={()=>props.toggle()} />
     </div>
   )
 };
--- torazemi/src/Blog.jsx
+++ torazemi/src/Blog.jsx
@@ -4,14 +4,22 @@ import Article from "./Article";
 class Blog extends React.Component{
   constructor(props){
     super(props);
+    this.state ={
+      isPublished: false
+    }
   }
+  // 公開状態を反転させる関数
+  togglePublished = () =>{
+    this.setState({
+      isPublished: !this.state.isPublished
+    })
+  };
+
   render(){
     const authorName = "Torahack";
     return (
       <>
-        <Article title={"Reactの使い方"} order={1} />
-        <Article title={"JSXの使い方"}order={2} />
-        <Article title={"環境変数の使い方"}order={3} />
+        <Article title={"Reactの使い方"} isPublished={this.state.isPublished} toggle={() =>this.togglePublished()} />
       </>
     )
   }
